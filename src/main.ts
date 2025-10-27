b95e3803b15f8d8450b5391de384b4d6ce3da0db
Felipe <felipediazb16@gmail.com>
2025-10-27 18:41:28 -0300

    Arreglado problema con las variables de entorno

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,7 @@ async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-<<<<<<< HEAD
     origin: ['http://localhost:3000', 'http://localhost:5173'],
-=======
-    origin: ['http://localhost:3000, http://localhost:5173'],
->>>>>>> 60806d5f6a1710da252949d175bafee3cdaee35c
     methods: 'GET,POST,PUT,DELETE',
     credentials: true,
   });
